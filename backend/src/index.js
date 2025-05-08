ad65f425f850b79d4c69245b16ab2758851ec4d5
anthony <fecotony@gmail.com>
2025-05-08 20:08:44 +0100

    node_modules deleted

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,10 +6,13 @@ import chatRoutes from "./routes/chat.route.js"
 import cookieparser from "cookie-parser"
 import dotenv from "dotenv"
 import {connectDB} from "./lib/db.js"
+import path from "path"
 
 dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5000
+
+const __dirname = path.resolve();
 
 app.use(
     cors({
@@ -25,6 +28,15 @@ app.use(cookieparser())
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 
 app.listen(PORT, () => {
     console.log("server running on port 5000")
